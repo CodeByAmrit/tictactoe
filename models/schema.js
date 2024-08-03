5ce13fa46944f0bc4528a46cee84038be18cae3a
CodeByAmrit <amritsharma54300@gmail.com>
2024-08-04 00:03:56 +0530

    addming database connection details

--- models/schema.js
+++ models/schema.js
@@ -1,10 +1,28 @@
 const mysql = require('mysql2/promise');
+require('dotenv').config()
+const fs = require('fs');
+const path = require('path');
 
 const con = mysql.createPool({
-  host: 'localhost',
+  host: 'mysql-game-tictactoe-amritsharma54300-7ee3.e.aivencloud.com',
   user: 'web',
-  password: 'Webuser@1234',
-  database: 'game'
+  password: process.env.password,
+  database: process.env.DATABASE,  // Ensure the correct environment variable is used
+  port: 23074,
+  ssl: {
+    ca: fs.readFileSync('/etc/secrets/ca.pem')
+  }
 });
+
+// async function runQuery() {
+//   try {
+//     const [rows, fields] = await con.query('SELECT * FROM users');
+//     console.log('Rows:', rows);
+//   } catch (error) {
+//     console.error('Query error:', error);
+//   }
+// }
+
+// runQuery();
 
 module.exports = { con };
--- models/structure.sql
+++ models/structure.sql
@@ -15,5 +15,11 @@ CREATE TABLE scoreSchema (
     name varchar(100),
     dateTime varchar(100),
     result varchar(100),
+    id int auto_increment primary key,
     FOREIGN KEY (playerId) REFERENCES users(userId)
 );
+
+CREATE USER 'web'@'%' IDENTIFIED  BY 'Amrit@123';
+
+grant select, insert, delete, update on *.* to 'web'@'%' ;
+flush privileges;
